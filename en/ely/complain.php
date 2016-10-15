e44ea67198ac44e4b6d30f50958a81883d78d663
GrinColdWright <error456_67@mail.ru>
2016-10-15 20:53:25 +0300

    Update complain.php
    Violation- это опустошение или насилие, (нарушение тоже но к правилам оно не подходит =D)

--- en/ely/complain.php
+++ en/ely/complain.php
@@ -2,6 +2,6 @@
 return [
     'button.go_to_form' => 'Go to form',
     'link.rules' => 'project rules',
-    'text.complain_about_content' => 'Please, tell us about {rulesLink} violation by sending information from our contact form.',
+    'text.complain_about_content' => 'Please, tell us about {rulesLink} rule breaking by sending information from our contact form.',
     'title.complain_about_content' => 'Complain about the content',
 ];
